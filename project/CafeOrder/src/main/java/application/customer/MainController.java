652eadb78ed4c951292e4422995579fd78996b5a
Hoon1999 <kjhoon44@gmail.com>
2024-09-12 10:12:40 +0900

    modify: 초기화면 - 상품주문 페이지 연결

--- project/CafeOrder/src/main/java/application/customer/MainController.java
+++ project/CafeOrder/src/main/java/application/customer/MainController.java
@@ -1,9 +1,15 @@
 package application.customer;
 
+import application.admin.main.AdminMainPageController;
+import application.admin.start.StartPage;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,8 +20,19 @@ public class MainController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //ToDo
     }
+    /**
+     * 상품 선택 페이지로 이동합니다.*/
     @FXML
     private void gotoPage() {
-        //ToDo
+        AdminMainPageController.resetMainPageController();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/application/customer/root.fxml"));
+            StartPage.getPrimaryStage().setScene(new Scene(root));
+            StartPage.getPrimaryStage().show();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
